2007f6727e00f47740ccae681b52d400df21ac09
shabeerahmed9840 <shabeerahmed1996@outlook.com>
2019-12-12 12:19:33 +0530

    i want to add it

---
+++ src/gitdemo/gittask.java
@@ -0,0 +1,18 @@
+package gitdemo;
+
+public class gittask {
+	public static void main(String[] args) {
+		String s=new String("india is my country");
+		int i = s.length();
+		System.out.println(i);
+		boolean b = s.startsWith("india");
+		System.out.println(b);
+		
+		
+		
+		
+		
+		
+	}
+
+}
